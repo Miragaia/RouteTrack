fb3cb8d2b6647af72a0dc6c49f3ab4329d815b72
Migas77 <miguel.belchior@ua.pt>
2024-10-23 16:46:32 +0100

    database setup

--- routertrack/lib/main.dart
+++ routertrack/lib/main.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:routertrack/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+import 'database/database.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final database = AppDatabase();
+  await database.into(database.todoItems).insert(TodoItemsCompanion.insert(
+    title: 'todo: finish drift setup',
+    content: 'We can now write queries and define our own tables.',
+  ));
+  List<TodoItem> allItems = await database.select(database.todoItems).get();
+  print('items in database: $allItems');
   await Firebase.initializeApp();
   runApp(MyApp());
 }
